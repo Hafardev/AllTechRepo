5240692ec3c670151a7b7a2b4930269497c6839a
Hafardev <farmanihadis@gmail.com>
2023-07-09 22:21:20 +0330

    -change and update new token github

--- app/src/main/java/com/example/mybasicapplication/di/ApiServiceModule.kt
+++ app/src/main/java/com/example/mybasicapplication/di/ApiServiceModule.kt
@@ -26,10 +26,8 @@ class ApiServiceModule {
         val headerMapParams: MutableMap<String, String> = mutableMapOf()
 
         headerMapParams.put("accept" ,"application/vnd.github+json")
-       /// headerMapParams.put("Authorization" ,"Bearer "+"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")//second OAuth Token (classic) with repo scope ticks - Expires on Fri, Mar 31 2023.
-        headerMapParams.put("Authorization" ,"Bearer "+"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")//Third OAuth Token (classic) with repo scope ticks - Expires on Fri, sep 03 2023.
+        headerMapParams.put("Authorization" ,"Bearer "+"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")//fourth OAuth Token (classic) with repo scope ticks - no expire.
         headerMapParams.put("X-GitHub-Api-Version" ,"2022-11-28")
-        // headerMapParams.put("Authorization" ,"Bearer "+"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")//first OAuth Token (classic)
 
         return RetrofitHelper.createRetrofit(baseUrl = "https://api.github.com" ,interceptorMap = interceptorsMap , headerMap = headerMapParams)
     }
